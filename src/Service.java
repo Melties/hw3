6e3c91e5f8e31ae4cd340f7d1805d052cbc83f78
Melties <131888470+Melties@users.noreply.github.com>
2023-06-11 23:49:54 +0300

    Update Service.java

--- src/Service.java
+++ src/Service.java
@@ -1,15 +1,27 @@
 public class Service {
 
     public Car changeCarColor(Car car, String newColor) {
-        car.changeColor(newColor);
-        return car;
+        if(car.getColor() == null || String.valueOf(car.getColor()) == newColor) {
+            return null;
+        } else {
+            car.changeColor(newColor);
+            return car;
+        }
     }
     public Car changeWheels(Car car, int newSize) {
-        car.changeWheelSize(newSize);
-        return car;
+        if(car.getWheelSize() == null || Integer.valueOf(car.getWheelSize().ordinal()) == newSize) {
+            return null;
+        } else {
+            car.changeWheelSize(String.valueOf(newSize));
+            return car;
+        }
     }
-    public Car changeCarOptions(Car car, String newOption) {
-        car.changeOptions(newOption);
-        return car;
+    public Car changeCarOptions(Car car, String[] newOption) {
+        if(car.getOptions() == null || String.valueOf(car.getOptions()) == String.valueOf(newOption)) {
+            return null;
+        } else {
+            car.changeOption(newOption);
+            return car;
+        }
     }
 }
